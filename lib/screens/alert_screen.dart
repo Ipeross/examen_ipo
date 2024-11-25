4ab3f00e359a7596040b9e5e5622d23229b90fd1
Pedro Bosch <pedro@andared.com>
2024-11-25 10:58:25 +0100

    AlertDialog

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,12 +3,41 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
+
+  void displayDialog(BuildContext context){
+    //print('Hola mundo');
+    showDialog(
+      barrierDismissible: true,
+      context: context, 
+      builder: ( context ) {
+        return AlertDialog(
+          title: Text('Alerta'),
+          content: 
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Este es el contenido del mensaje de alerta'),
+              SizedBox(height: 10,),
+              FlutterLogo( size:  100,)
+            ],
+          ),
+          
+        );
+      }
+    );
+  }
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-         child: Text('AlertScreen'),
+      body: Center(
+         child: ElevatedButton(
+          onPressed: () => displayDialog(context), 
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: const Text('Mostrar alerta', style: TextStyle(fontSize: 20),),
+          )
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.close),
