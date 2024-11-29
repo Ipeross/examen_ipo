8903d35c0541c96a0617604bf2c70742d9a3183c
Pedro Bosch <pedro@andared.com>
2024-11-29 10:14:35 +0100

    Foco en el boton enviar

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -7,46 +7,73 @@ class InputsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    final myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'nombre':  'Andres',
+      'apellido': 'Iniesta',
+      'email': 'iniesta@gmail.com',
+      'password': '123456',
+      'role': 'usuario'
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Forms: Inputs'),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-         child: Column(
-          children: [
-            CustomTextFormField(
-              hintText: 'Nombre',
-              labelText: 'Nombre del usuario',
-              helperText: 'Solo letras',
-              icon: Icons.verified_outlined,
-              suffixIcon: Icons.person_2_rounded,
-              obscureText: false,
-            ),
-            SizedBox(height: 30),
-             CustomTextFormField(
-              hintText: 'Apellidos',
-              labelText: 'Apellidos del usuario',
-              icon: Icons.person_4_outlined,
-              obscureText: false,
-            ),
-            SizedBox(height: 30),
-             CustomTextFormField(
-              hintText: 'E-Mail',
-              labelText: 'E-Mail del usuario',
-              icon: Icons.email_rounded,
-              keyboardType: TextInputType.emailAddress,
-              obscureText: false,
-            ),
-            SizedBox(height: 30),
-             CustomTextFormField(
-              hintText: 'Contraseña',
-              labelText: 'Contraseña del usuario',
-              icon: Icons.password_rounded,
-              obscureText: true,
-            ),
-            
-          ],
+         child: Form(
+          key: myFormKey,
+           child: Column(
+            children: [
+              CustomTextFormField(
+                hintText: 'Nombre',
+                labelText: 'Nombre del usuario',
+                helperText: 'Solo letras',
+                icon: Icons.verified_outlined,
+                suffixIcon: Icons.person_2_rounded,
+                obscureText: false,
+              ),
+              SizedBox(height: 30),
+               CustomTextFormField(
+                hintText: 'Apellidos',
+                labelText: 'Apellidos del usuario',
+                icon: Icons.person_4_outlined,
+                obscureText: false,
+              ),
+              SizedBox(height: 30),
+               CustomTextFormField(
+                hintText: 'E-Mail',
+                labelText: 'E-Mail del usuario',
+                icon: Icons.email_rounded,
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+              ),
+              SizedBox(height: 30),
+               CustomTextFormField(
+                hintText: 'Contraseña',
+                labelText: 'Contraseña del usuario',
+                icon: Icons.password_rounded,
+                obscureText: true,
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                  if(!myFormKey.currentState!.validate()){
+                    print('Fomulario no valido');
+                    return;
+                  }
+                }, 
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Center(child: Text('Enviar'))
+                  )
+              )
+            ],
+           ),
          ),
       ),
     );
